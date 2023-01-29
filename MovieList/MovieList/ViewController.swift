e3de76624c199461bfbaed06099d346aa5c98c89
Sara Felix <sfelix@lifescan.com>
2023-01-29 17:10:52 -0300

    start project

---
+++ MovieList/MovieList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieList
+//
+//  Created by Sara Batista dos Santos Felix on 1/29/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
