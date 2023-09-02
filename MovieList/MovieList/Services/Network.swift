6639ff6358b0e38ef64d2cc91131b946f6de5356
Sara Felix <sfelix@MacBook-Air-de-Sara.local>
2023-09-02 18:38:42 -0300

    melhorias

--- MovieList/MovieList/Services/Network.swift
+++ MovieList/MovieList/Services/Network.swift
@@ -11,7 +11,7 @@ class Network {
     static let shared = Network()
     
     func fetchPopularMovies(_ completion: @escaping ([Movie]) -> ()) {
-        guard let url = URL(string: "https://api.themoviedb.org/3/movie/popular?api_key=5e8557173983a24a1c20ae9e96b47c49&language=pt-BR&page=1") else { return }
+        guard let url = URL(string: "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=d8b5cf6c67f7e8e12cd083267f095929") else { return }
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
             guard let data = data else {
                 return
